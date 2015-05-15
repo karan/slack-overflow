29a1599bf6af24dec1d12c4d34eae4f3827dc17f
Karan Goel <karan@goel.io>
2015-05-15 14:10:04 -0700

    add emoji

--- app.py
+++ app.py
@@ -61,7 +61,8 @@ def overflow():
                         '<https://stackoverflow.com|StackOverflow>.'))
 
     resp_qs.append(('\n<https://donorbox.org/karangoel-karan-s-college-fund|'
-                    'Contribute to Karan\'s College Fund Piggy Bank>'))
+                    ':innocent: Contribute to Karan\'s College '
+                    'Fund Piggy Bank :innocent:>'))
 
     return Response('\n'.join(resp_qs), content_type='text/plain; charset=utf-8')
 
