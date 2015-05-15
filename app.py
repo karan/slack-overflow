0e612c56770e8fedda8898491158c14d0fe759ae
Karan Goel <karan@goel.io>
2015-05-15 14:58:12 -0700

    disable debugging

--- app.py
+++ app.py
@@ -83,5 +83,5 @@ def hello():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=port, debug=True)
